c8f18e736b5d95554819bb9c7bf2d6e45b06b0f2
Esteban Sánchez <esteban.sanchez@bravent.net>
2024-04-18 16:54:26 +0200

    mc

--- pages/bia.py
+++ pages/bia.py
@@ -102,8 +102,8 @@ for i in range(num_rows):
 
 
 st.markdown(body="## :telephone_receiver: Contacto")
-st.markdown('#### Únete a nosotros: ')
-st.write('talentmanagement@bravent.net')
+st.markdown('#### Únete a nosotros ')
+st.markdown('###### talentmanagement@bravent.net')
 
 st.markdown('#### ¿Algún proyecto en mente? Innova con nosotros')
-st.write('eduardo.garcia@bravent.net')
+st.markdown('###### eduardo.garcia@bravent.net')
